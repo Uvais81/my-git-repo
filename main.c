e3cbb17ba42b252876f633e6f6005c8a7d6091a4
Uvais81 <mohammad12.uvais@routerarchitects.com>
2024-12-27 16:06:12 +0530

    Second commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int a, b, sum;
+	printf("Enter the value of a:\n");
+	scanf("%d",&a);
+	printf("Enter the value of b:\n");
+	scanf("%d",&b);
+	sum = a + b;
+	printf("Sum of %d and %d is : %d\n",a,b,sum);
+	return 0;
+}
